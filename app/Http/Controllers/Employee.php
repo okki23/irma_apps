a167697cc0ffa837439b7b65b503f39437ddaeb0
okki23 <okkisetyawan@gmail.com>
2021-06-01 16:08:26 +0700

    update lagi

--- app/Http/Controllers/Employee.php
+++ app/Http/Controllers/Employee.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EmployeeModel;
 
 class Employee extends Controller
 {
     //
+    public function __construct(){
+      $this->EmployeeModel = new EmployeeModel();
+    }
 
-    public function index() {
+    public function index() { 
         echo 1;
- 
     }
 }
